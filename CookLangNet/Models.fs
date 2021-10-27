29661739cfa2653f318d8318815a79d1c5cfb2ab
William Barbosa <heytherewill@gmail.com>
2021-10-27 07:11:44 +0200

    🚧 Crude domain modeling

---
+++ CookLangNet/Models.fs
@@ -0,0 +1,40 @@
+﻿namespace CooklangNet
+
+open System.Collections.Generic
+
+type UnitOfMeasurement = string
+
+type IngredientAmount = {
+    quantity: float
+    unit: UnitOfMeasurement
+}
+
+type Ingredient = {
+    name: string
+    amount: IngredientAmount
+}
+
+type Equipment = {
+    name: string
+}
+
+type Direction = {
+    description: string
+}
+
+type Timer = {
+    quantity: float
+    unit: string
+}
+
+type Step = {
+    timers: Timer list
+    directions: Direction list
+}
+
+type Recipe = {
+    steps: Step list
+    ingredients: Ingredient list
+    neededEquipment: Equipment list
+    metadata: IDictionary<string, string>
+}
